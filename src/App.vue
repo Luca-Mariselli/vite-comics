4378eeaa07c045455615cb6fd2dbe062a5bfbc22
Luca-Mariselli <lucamariselli@gmail.com>
2024-05-23 17:14:39 +0200

    Inserito AppMain

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-// import AppMain from './components/AppMain.vue';
+import AppMain from './components/AppMain.vue';
 // import AppFooter from './components/AppFooter.vue';
 
 export default {
   components: {
     AppHeader,
+    AppMain,
 
   },
   data() {
@@ -24,8 +25,8 @@ export default {
 
   <AppHeader />
 
-  <main>
-
+  <main class="boxed">
+    <AppMain />
   </main>
 
   <!-- <AppFooter /> -->
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+
+export default {
+
+}
+
+</script>
+
+<template>
+    <main>
+        <h1 class="p-5"> -- Content goes here <-- </h1>
+    </main>
+</template>
+
+<style scoped></style>
